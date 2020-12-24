9e9db03f5adbd493b6d50d98a164fa125ee5190e
mickeyys26 <76561668+mickeyys26@users.noreply.github.com>
2020-12-24 20:55:12 +0700

    Delete connection.php

--- connection.php
+++
@@ -1,8 +0,0 @@
-<?php
-$server_username = "root";
-$server_password = "ABC123";
-$server_host = "localhost";
-$database = "kungfuphp";
-
-$conn = mysqli_connect($server_host,$server_username,$server_password,$database) or die("không thể kết nối tới database");
-mysqli_query($conn,"SET NAMES 'UTF8'");
